eb4351a37244993b9e375ebb7f5ee3d7daa6002f
Valentine <v4lbuild3r@gmail.com>
2025-05-21 22:48:17 -0500

    export function + footer

--- src/lib/Export.svelte
+++ src/lib/Export.svelte
@@ -1,5 +1,35 @@
 <script lang="ts">
+  import type {
+    DriverPositionInfo,
+    DispatcherPositionInfo,
+    GuardPositionInfo,
+    SignallerPositionInfo,
+    SupervisorPositionInfo,
+  } from "$lib";
+
+  let {
+    qdPos = undefined,
+    dsPos = undefined,
+    gdPos = undefined,
+    sgPos = undefined,
+    svPos = undefined,
+  }: {
+    qdPos?: DriverPositionInfo[];
+    dsPos?: DispatcherPositionInfo[];
+    gdPos?: GuardPositionInfo[];
+    sgPos?: SignallerPositionInfo[];
+    svPos?: SupervisorPositionInfo[];
+  } = $props();
+
   import { Button } from "flowbite-svelte";
+
+  let full = {
+    driver: qdPos,
+    dispatcher: dsPos,
+    guard: gdPos,
+    signaller: sgPos,
+    supervisor: svPos,
+  };
 </script>
 
 <div
@@ -8,7 +38,17 @@
   <Button
     color="light"
     onclick={() => {
-      alert("Coming soon:tm:");
-    }}>Export</Button
+      const dataStr =
+        "data:text/json;charset=utf-8," +
+        encodeURIComponent(JSON.stringify(full, null, 2));
+      const dlAnchorElem = document.createElement("a");
+      dlAnchorElem.setAttribute("href", dataStr);
+      dlAnchorElem.setAttribute("download", "positions.json");
+      document.body.appendChild(dlAnchorElem);
+      dlAnchorElem.click();
+      document.body.removeChild(dlAnchorElem);
+    }}
   >
+    Export
+  </Button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,59 +14,36 @@
 </script>
 
 <div class="min-h-screen bg-slate-800 py-4 dark">
-  {#if (driverPositions.length == 0 && dispatcherPositions.length == 0 && guardPositions.length == 0 && signallerPositions.length == 0 && supervisorPositions.length == 0) || supervisorPositions.length == 0}
-    <div
-      class="bg-slate-700 max-w-screen-xl mx-auto rounded-tl-none rounded-xl p-4 text-white mb-4"
-    >
-      {#if driverPositions.length == 0 && dispatcherPositions.length == 0 && guardPositions.length == 0 && signallerPositions.length == 0 && supervisorPositions.length == 0}
-        <div
-          class="bg-red-900 border border-red-500 rounded-xl rounded-tl-none my-2 p-2"
-        >
-          <p>No positions added. You must add at least one.</p>
-        </div>
-      {/if}
-
-      {#if supervisorPositions.length == 0}
-        <div
-          class="bg-red-900 border border-red-500 rounded-xl rounded-tl-none my-2 p-2"
-        >
-          <p>
-            No supervisor positions added. You must add at least one, make sure
-            to add one for yourself!
-          </p>
-        </div>
-      {/if}
-
-      {#if driverPositions.length + dispatcherPositions.length + guardPositions.length + signallerPositions.length + supervisorPositions.length > 75}
-        <div
-          class="bg-red-900 border border-red-500 rounded-xl rounded-tl-none my-2 p-2"
-        >
-          <p>
-            Too many positions. You currently have {driverPositions.length +
-              dispatcherPositions.length +
-              guardPositions.length +
-              signallerPositions.length +
-              supervisorPositions.length} positions. The maximum is 75.
-          </p>
-        </div>
-      {/if}
-
-      {#if driverPositions.length + dispatcherPositions.length + guardPositions.length + signallerPositions.length + supervisorPositions.length == 75}
-        <div
-          class="bg-yellow-900 border border-yellow-500 rounded-xl rounded-tl-none my-2 p-2"
-        >
-          <p>
-            Maximum number of positions reached. You can only have 75 positions.
-          </p>
-        </div>
-      {/if}
-    </div>
-  {/if}
-
   <Drivers bind:positions={driverPositions} />
   <Dispatchers bind:positions={dispatcherPositions} />
   <Guards bind:positions={guardPositions} />
   <Signallers bind:positions={signallerPositions} />
   <Supervisors bind:positions={supervisorPositions} />
-  <Export />
+  <Export
+    qdPos={driverPositions}
+    dsPos={dispatcherPositions}
+    gdPos={guardPositions}
+    sgPos={signallerPositions}
+    svPos={supervisorPositions}
+  />
+
+  <div
+    class="bg-slate-700 max-w-screen-xl mx-auto rounded-tl-none rounded-xl p-4 text-white mb-4 dark"
+  >
+    <p>
+      Made with ❤️ by <a href="https://valbuilds.xyz/" class="hover:underline"
+        >Valentine</a
+      > 🏳️‍⚧️
+    </p>
+    <p>
+      This project has no affiliation with Stepford County Railway or it's
+      staff. Please direct any inquires related to SCR to the SCR staff.
+    </p>
+    <p>
+      This project is open source! View the source code <a
+        href="https://github.com/valbuildr/scr-shift-positions-json-creator"
+        class="hover:underline">here</a
+      >.
+    </p>
+  </div>
 </div>
